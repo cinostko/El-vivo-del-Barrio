3c91e67efdc2580d5541087d7cb2119f785f5ac6
unknown <sergiorojas0123@gmail.com>
2024-05-10 00:49:05 -0500

    script de movimiento

--- Assets/scripts/move.cs
+++ Assets/scripts/move.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float velocidad = 5f;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(horizontal, vertical, 0) * Time.deltaTime * velocidad;
+
+
     }
 }
